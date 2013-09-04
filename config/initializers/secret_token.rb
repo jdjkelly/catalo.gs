a481375cac5865670764b7ade77b68148c2157c4
Joshua Kelly <jdjkelly@gmail.com>
2013-09-04 10:28:07 -0400

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Catalogs::Application.config.secret_token = 'b60b92e195b4e602d1baac6d37bf044cbcfc9bfefbdd0323b311137436bf0d3b58f4b5bc3b974175289d92b2649196285ae98a4c580487526708f25d36475298'
+Catalogs::Application.config.secret_token = ENV['secret_token']
